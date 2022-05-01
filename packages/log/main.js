8d2aed6a4026247e110ca3f0fd3ceed5eaac6c31
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-05-01 12:10:18 +0530

    fix(root): testing to remove NPM token from env secrets

--- packages/log/main.js
+++ packages/log/main.js
@@ -2,4 +2,4 @@ console.log('hello from my package');
 
 console.log('new change from feature branch, new changes');
 
-console.log('pkg changes');
+console.log('remove-npm-token-from-env-secrets-test');
